59b50b1de9fcb7dec4a684c5645bc23bd6053a89
Wei Fan <zxvbnm456@gmail.com>
2022-07-22 00:17:13 +0800

    done chat.py

---
+++ chat.py
@@ -0,0 +1,38 @@
+def read_file(filename):
+    lines = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+
+def convert(lines):
+    new = []
+    person = None
+    for line in lines:
+        if line in lines:
+            if line == 'Mike':
+                person = 'Mike'
+                continue
+            elif line == 'Kevin':
+                person = 'Kevin'
+                continue
+        if person:
+            new.append(person+': '+line)
+    return new
+    # print(new)
+
+
+def write_file(filename, lines):
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line + '\n')
+
+
+def main():
+    lines = read_file('input.txt')
+    lines = convert(lines)
+    write_file('output.txt', lines)
+
+
+main()
